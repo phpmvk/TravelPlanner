37472cbda11ee8ac814e0875beb089989f729925
GuillaumeRcd <120723419+GuillaumeRcd@users.noreply.github.com>
2023-04-22 11:52:29 +0200

    displaying activities and journeyx in order inside modify components

--- Client/src/Components/Modifiy/Modify.jsx
+++ Client/src/Components/Modifiy/Modify.jsx
@@ -1,33 +1,34 @@
-import { Link } from 'react-router-dom';
-import './Modify.css'
-import { getTripsByUser/*, getActivitiesByTripId */} from "../../api.service"
-import { useState } from 'react';
+import { Link } from "react-router-dom";
+import "./Modify.css";
+import { getTripsByUser /*, getActivitiesByTripId */ } from "../../api.service";
+import { useState } from "react";
+import moment from "moment";
 
 function Modify() {
   const [trips, setTrips] = useState([]);
 
-  const handleSubmit = async function(e){
+  const handleSubmit = async function (e) {
     e.preventDefault();
-    
+
     const searchedUser = {
-        user: e.target[0].value,
-    }
+      user: e.target[0].value,
+    };
 
     const constructSearchUrl = function () {
-        const arrRes = ["http://localhost:3001/modify?user="];
-        arrRes.push(searchedUser.user);
-        return arrRes.join("");
+      const arrRes = ["http://localhost:3001/modify?user="];
+      arrRes.push(searchedUser.user);
+      return arrRes.join("");
     };
-    
+
     const url = constructSearchUrl();
 
     const resultOfSearch = await getTripsByUser(url);
 
     setTrips(resultOfSearch);
-    
-    e.target.reset()
-  }
-  
+
+    e.target.reset();
+  };
+
   const renderTrips = () => {
     if (trips.length === 0) {
       return null;
@@ -41,6 +42,106 @@ function Modify() {
           <p>Arrival City: {trip.arrCity}</p>
           <p>Budget: {trip.budget}</p>
           <p>Duration: {trip.duration} days</p>
+
+          {/* <h3>Journeys</h3> */}
+          {/* <ul>
+            {trip.journeys.map((journey) => (
+              <div className="journey-container">
+                <li key={journey.id}>
+                <h3>{journey.transportType.charAt(0).toUpperCase() + journey.transportType.slice(1)} to {journey.arrCity.charAt(0).toUpperCase() + journey.arrCity.slice(1)}</h3>
+                  <p>
+                    Start: {moment(journey.start).format("DD MMMM YYYY, HH:mm")}
+                  </p>
+                  <p>
+                    End: {moment(journey.end).format("DD MMMM YYYY, HH:mm")}
+                  </p>
+                  <p>Departure City: {journey.depCity}</p>
+                  <p>Arrival City: {journey.arrCity}</p>
+                  <p>Price: {journey.price}</p>
+                </li>
+              </div>
+            ))}
+          </ul> */}
+
+          {/* <h3>Activities</h3> */}
+          {/* <ul>
+            {trip.activities.map((activity) => (
+              <div className="activity-container">
+                <li key={activity.id}>
+                  <h3>
+                    {activity.activityType.charAt(0).toUpperCase() +
+                      activity.activityType.slice(1)}
+                  </h3>
+                  <p>
+                    Start:{" "}
+                    {moment(activity.start).format("DD MMMM YYYY, HH:mm")}
+                  </p>
+                  <p>
+                    End: {moment(activity.end).format("DD MMMM YYYY, HH:mm")}
+                  </p>
+                  <p>Departure City: {activity.depCity}</p>
+                  {activity.arrCity && <p>Arrival City: {activity.arrCity}</p>}
+                  <p>Price: {activity.price}</p>
+                  {activity.additionalInfo && (
+                    <p>Additional Info: {activity.additionalInfo}</p>
+                  )}
+                </li>
+              </div>
+            ))}
+          </ul> */}
+
+          {trip.journeys
+            .concat(trip.activities)
+            .sort((a, b) => new Date(a.start) - new Date(b.start))
+            .map((item) => (
+              <div>
+                {item.transportType ? (
+                  <div className="journey-container" key={item.id}>
+                    <li>
+                      <h3>
+                        {item.transportType.charAt(0).toUpperCase() +
+                          item.transportType.slice(1)}{" "}
+                        to{" "}
+                        {item.arrCity.charAt(0).toUpperCase() +
+                          item.arrCity.slice(1)}
+                      </h3>
+                      <p>
+                        Start:{" "}
+                        {moment(item.start).format("dddd HH:mm")}
+                      </p>
+                      <p>
+                        End: {moment(item.end).format("dddd HH:mm")}
+                      </p>
+                      <p>Departure City: {item.depCity}</p>
+                      <p>Arrival City: {item.arrCity}</p>
+                      <p>Price: {item.price}</p>
+                    </li>
+                  </div>
+                ) : (
+                  <div className="activity-container" key={item.id}>
+                    <li>
+                      <h3>
+                        {item.activityType.charAt(0).toUpperCase() +
+                          item.activityType.slice(1)}
+                      </h3>
+                      <p>
+                        Start:{" "}
+                        {moment(item.start).format("dddd HH:mm")}
+                      </p>
+                      <p>
+                        End: {moment(item.end).format("dddd HH:mm")}
+                      </p>
+                      <p>Departure City: {item.depCity}</p>
+                      {item.arrCity && <p>Arrival City: {item.arrCity}</p>}
+                      <p>Price: {item.price}</p>
+                      {item.additionalInfo && (
+                        <p>Additional Info: {item.additionalInfo}</p>
+                      )}
+                    </li>
+                  </div>
+                )}
+              </div>
+            ))}
         </div>
       );
     });
@@ -51,7 +152,9 @@ function Modify() {
       <form onSubmit={handleSubmit}>
         <h4>UserName</h4>
         <input className="inputs" placeholder="Name"></input>
-        <button className="button" type="submit">Create</button>
+        <button className="button" type="submit">
+          Create
+        </button>
       </form>
 
       {renderTrips()}
@@ -63,5 +166,4 @@ function Modify() {
   );
 }
 
-export default Modify
-
+export default Modify;
